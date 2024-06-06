7ace9b8a2b020bcab0fd978074e614a604242eb9
OrlandoMts <dani_monts19@hotmail.com>
2024-06-05 23:49:48 -0600

    2. Connection db, start auth module and configure project

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,21 @@
+import { join } from 'path';
+
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { AuthModule } from './auth/auth.module';
+import { EnvConfiguration } from './config/config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ load: [EnvConfiguration] }),
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public/browser'),
+    }),
+    MongooseModule.forRoot(process.env.MONGODB, { dbName: 'todo' }),
+    AuthModule,
+  ],
 })
 export class AppModule {}
